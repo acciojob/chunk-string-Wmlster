f92e7aa11c781e22a3af73f35fd38ce472006682
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-22 13:31:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-function stringChop(s, size) {          
+function stringChop(s, size) {       
   // your code here
 
 	let n=s.length;
 
 	size=parseInt(size);
- 
+
+
+	if(n==0)
+		return [];
+
+	let count=0;
 
 	let v=[];
 
-	if(s===null)
-	{
-		return v[""];
-	}
-
-
-	for(let i=0;i<n;i+=size)  
+		for(let i=0;i<n;i+=(size))  
 		{
-			let n1=i+size;
+			let n1=i+(size);
 
 			if(n1>=n)
 			{
 				n1=n;
 			}
 
-			let s1=s.substring(i,n1);
+//console.log(i+"  "+n1);
+
+			let s1=s.substring((i),(n1));
 			
 			v.push(s1);
 		}
 
-
-	return v 
+	return v;
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
+c/onst str = prompt("Enter String.");
+co/nst size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
