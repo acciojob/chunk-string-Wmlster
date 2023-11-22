50c2f5712f19070383470438547b1338aee100e8
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-22 18:38:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,29 +5,11 @@ function stringChop(s, size) {
 
 	let v=[];
 
-	if(n==0)
+	if(n==0||size==null)
 	{
 		return v;
 	}
 	
-size=parseInt(size);
-
-	for(let i=0;i<n;i+=(size))  
-		{
-			let n1=i+(size);
-
-			if(n1>=n)
-			{
-				n1=n;
-			}
-
-			let s1=s.substring((i),(n1));
-			
-			v.push(s1);
-		}
-
-
-	return v;
 }
 
 // Do not change the code below
